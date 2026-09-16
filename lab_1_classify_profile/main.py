193f74a99081a0f07f23e3fae0a46953c9c7668a
unknown <vlad.malyshev.20007@mail.ru>
2026-09-16 20:32:17 +0300

    Fixing changes x3

--- lab_1_classify_profile/main.py
+++ lab_1_classify_profile/main.py
@@ -61,11 +61,11 @@ def remove_stop_words(tokens: Sequence[str], stop_words: Sequence[str]) -> Seque
         return None
 
     if not isinstance(stop_words, (list, tuple)):
-        return tokens
+        return None
 
     for word in stop_words:
         if not isinstance(word, str):
-            return tokens
+            return None
 
     result = []
 
